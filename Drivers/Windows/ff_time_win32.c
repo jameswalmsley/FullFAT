cab67c0497711df8665f804f7c214a48fa5f4476
James Walmsley <james@fullfat-fs.co.uk>
2011-07-30 18:51:45 +0000

    License headers for Windows drivers.

--- Drivers/Windows/ff_time_win32.c
+++ Drivers/Windows/ff_time_win32.c
@@ -1,3 +1,22 @@
+/*****************************************************************************
+ *  FullFAT - High Performance, Thread-Safe Embedded FAT File-System         *
+ *                                                                           *
+ *  Copyright(C) 2009 James Walmsley  (james@fullfat-fs.co.uk)               *
+ *	Copyright(C) 2010 Hein Tibosch    (hein_tibosch@yahoo.es)                *
+ *                                                                           *
+ *  See RESTRICTIONS.TXT for extra restrictions on the use of FullFAT.       *
+ *                                                                           *
+ *  Removing this notice is illegal and will invalidate this license.        *
+ *****************************************************************************
+ *  See http://www.fullfat-fs.co.uk/ for more information.                   *
+ *  Or  http://fullfat.googlecode.com/ for latest releases and the wiki.     *
+ *****************************************************************************
+ * As of 19-July-2011 FullFAT has abandoned the GNU GPL License in favour of *
+ * the more flexible Apache 2.0 license. See License.txt for full terms.     *
+ *                                                                           *
+ *            YOU ARE FREE TO USE FULLFAT IN COMMERCIAL PROJECTS             *
+ *****************************************************************************/
+
 #include "../../src/ff_time.h"
 #include <windows.h>
 
