f0782c724edb4220e9490effa09b0aa5ff2c22fe
James Walmsley <james@fullfat-fs.co.uk>
2012-05-05 13:45:37 +0100

    Added new testsuite system for verification.

---
+++ testsuite/verification/src/verification.h
@@ -0,0 +1,29 @@
+#ifndef _VERIFICATION_H_
+#define _VERIFICATION_H_
+
+#include <fullfat.h>
+
+#define FAIL 0
+#define PASS 1
+
+typedef int (*TEST_FUNCTION) (FF_IOMAN *pIoman, char **pszpMessage);
+
+typedef struct {
+	const char 		*szpTestName;			///< Name of the test.
+	const char 		*szpTestDescription;	///< Description of the test.
+	const char 		*szpTestAuthor;			///< Who wrote the test.
+	TEST_FUNCTION 	 pfnTest;				///< Test entry point.
+} VERIFICATION_TEST;
+
+
+typedef struct {
+	const char 		*szpModuleName;
+	const char 		*szpModuleDescription;
+	const char 		*szpModuleAuthor;
+	int nTests; 				///< Number of tests in this module.
+	const VERIFICATION_TEST *pTests;	///< Pointer to an array of tests.
+} VERIFICATION_INTERFACE;
+
+const VERIFICATION_INTERFACE *(*FN_GET_VERIFICATION_IF)();
+
+#endif
